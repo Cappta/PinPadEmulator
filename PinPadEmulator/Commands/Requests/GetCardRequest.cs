28acf85603c72ce8f9a451718c662db8b8746403
Sérgio Fonseca <sergio.fonseca@cappta.com.br>
2017-09-11 18:08:38 -0300

    Refactored GetCardRequest from 12h (hh) time to 24h (HH) time

--- PinPadEmulator/Commands/Requests/GetCardRequest.cs
+++ PinPadEmulator/Commands/Requests/GetCardRequest.cs
@@ -9,7 +9,7 @@ namespace PinPadEmulator.Commands.Requests
 		public readonly FixedLengthField<int> AcquirerId = new FixedLengthField<int>(2);
 		public readonly FixedLengthField<int> TargetAid = new FixedLengthField<int>(2);
 		public readonly FixedLengthField<int> TransactionAmount = new FixedLengthField<int>(12);
-		public readonly DateTimeField TransactionDateTime = new DateTimeField("yyMMddhhmmss");
+		public readonly DateTimeField TransactionDateTime = new DateTimeField("yyMMddHHmmss");
 		public readonly FixedLengthField<string> TableVersion = new FixedLengthField<string>(10);
 		public readonly FieldList<AidEntryReference> AidEntryReferences = new FieldList<AidEntryReference>(2);
 		public readonly FixedLengthField<bool?> ContactlessOn = new FixedLengthField<bool?>(1).Optional();

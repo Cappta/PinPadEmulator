6025b8dc5eaf13a0a4090e50e964632943b0beec
rmterra <r.mirandaterra@gmail.com>
2017-09-21 13:39:19 -0300

    MAE103 Fix GetPinRequest field mapping

--- PinPadEmulator/Commands/Requests/GetPinRequest.cs
+++ PinPadEmulator/Commands/Requests/GetPinRequest.cs
@@ -9,7 +9,7 @@ namespace PinPadEmulator.Commands.Requests
 		public FixedLengthField<CryptoMethod> Method { get; } = new FixedLengthField<CryptoMethod>(1);
 		public FixedLengthField<int> MasterKeyIndex { get; } = new FixedLengthField<int>(2);
 		public FixedLengthField<byte[]> EncryptedWorkingKey { get; } = new FixedLengthField<byte[]>(32);
-		public VariableLengthField<string> Pan { get; } = new VariableLengthField<string>(2, 19);
+		public PaddedVariableLengthField<string> Pan { get; } = new PaddedVariableLengthField<string>(2, 19);
 		public FieldList<GetPinEntry> Entries { get; } = new FieldList<GetPinEntry>(1);
 	}
 }

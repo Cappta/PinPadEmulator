49c9c5ba0d3a75bec1a32a69f16d6f85594c9f5a
rmterra <r.mirandaterra@gmail.com>
2017-09-28 18:35:26 -0300

    MAE103 Fix variable byte array fields handling

--- PinPadEmulator/Fields/PaddedVariableLengthField.cs
+++ PinPadEmulator/Fields/PaddedVariableLengthField.cs
@@ -4,35 +4,36 @@ using System;
 
 namespace PinPadEmulator.Fields
 {
-	public class PaddedVariableLengthField<type> : VariableLengthField<type>
-	{
-		public PaddedVariableLengthField(int headerLength, int contentLength) : base(headerLength)
-		{
-			this.ContentLength = contentLength;
-		}
+    public class PaddedVariableLengthField<type> : VariableLengthField<type>
+    {
+        public PaddedVariableLengthField(int headerLength, int contentLength) : base(headerLength)
+        {
+            this.ContentLength = contentLength;
+        }
 
-		public int ContentLength { get; }
+        public int ContentLength { get; }
 
-		private int TotalLength { get { return this.HeaderLength + this.ContentLength; } }
+        private int TotalLength { get { return this.HeaderLength + this.ContentLength; } }
 
-		public override void Init(StringReader stringReader)
-		{
-			var headerContent = stringReader.Read(this.HeaderLength);
-			var contentLength = headerContent.ConvertTo<int>();
+        public override void Init(StringReader stringReader)
+        {
+            int contentLength = ReadContentLength(stringReader);
 
-			var content = stringReader.Read(contentLength);
-			this.Value = content.ConvertTo<type>();
+            var content = stringReader.Read(contentLength);
+            this.Value = content.ConvertTo<type>();
 
-			stringReader.Seek(this.ContentLength - content.Length);
-		}
+            stringReader.Seek(this.ContentLength - content.Length);
+        }
 
-		public override string ToString()
-		{
-			var converted = base.ToString();
+        public override string ToString()
+        {
+            var converted = base.ToString();
 
-			if(converted.Length > this.TotalLength) { throw new ArgumentOutOfRangeException($"Exceeded field length"); }
+            if (converted.Length > this.TotalLength) { throw new ArgumentOutOfRangeException($"Exceeded field length"); }
 
-			return converted.PadRight(this.TotalLength, ' ');
-		}
-	}
+            return typeof(type) == typeof(byte[])
+                ? converted.PadRight(this.TotalLength, '0')
+                : converted.PadRight(this.TotalLength, ' ');
+        }
+    }
 }
--- PinPadEmulator/Fields/VariableLengthField.cs
+++ PinPadEmulator/Fields/VariableLengthField.cs
@@ -5,41 +5,58 @@ using System.Text;
 
 namespace PinPadEmulator.Fields
 {
-	public class VariableLengthField<type> : Field<type>
-	{
-		public VariableLengthField(int headerLength, Nullable<int> maximumContentLength = default(Nullable<int>))
-		{
-			this.HeaderLength = headerLength;
-			this.MaximumContentLength = maximumContentLength;
-		}
+    public class VariableLengthField<type> : Field<type>
+    {
+        public VariableLengthField(int headerLength)
+        {
+            this.HeaderLength = headerLength;
+        }
 
-		public int HeaderLength { get; }
-		public Nullable<int> MaximumContentLength { get; }
+        public VariableLengthField(int headerLength, int maximumContentLength)
+            : this(headerLength)
+        {
+            this.MaximumContentLength = maximumContentLength;
+        }
 
-		public override void Init(StringReader stringReader)
-		{
-			var headerContent = stringReader.Read(this.HeaderLength);
-			var contentLength = headerContent.ConvertTo<int>();
+        public virtual int HeaderLength { get; }
 
-			var content = stringReader.Read(contentLength);
+        public Nullable<int> MaximumContentLength { get; }
 
-			if (this.MaximumContentLength.HasValue && content.Length > this.MaximumContentLength)
-			{
-				throw new ArgumentOutOfRangeException($"This field has a limit of {this.MaximumContentLength}");
-			}
+        public override void Init(StringReader stringReader)
+        {
+            int contentLength = ReadContentLength(stringReader);
 
-			this.Value = content.ConvertTo<type>();
-		}
+            var content = stringReader.Read(contentLength);
 
-		public override string ToString()
-		{
-			var converted = base.ToString();
+            if (this.MaximumContentLength.HasValue && content.Length > this.MaximumContentLength)
+            {
+                throw new ArgumentOutOfRangeException($"This field has a limit of {this.MaximumContentLength}");
+            }
 
-			var stringBuilder = new StringBuilder();
-			stringBuilder.Append(converted.Length.ToString(this.HeaderLength));
-			stringBuilder.Append(converted);
+            this.Value = content.ConvertTo<type>();
+        }
 
-			return stringBuilder.ToString();
-		}
-	}
+        protected int ReadContentLength(StringReader stringReader)
+        {
+            var headerContent = stringReader.Read(this.HeaderLength);
+            var header = headerContent.ConvertTo<int>();
+
+            return typeof(type) == typeof(byte[]) ? header * 2 : header;
+        }
+
+        public override string ToString()
+        {
+            var converted = base.ToString();
+            var header = this.CalculateHeaderLength(converted);
+
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append(header.ToString(this.HeaderLength));
+            stringBuilder.Append(converted);
+
+            return stringBuilder.ToString();
+        }
+
+        protected int CalculateHeaderLength(string converted)
+             => typeof(type) == typeof(byte[])? converted.Length / 2 : converted.Length;
+    }
 }
